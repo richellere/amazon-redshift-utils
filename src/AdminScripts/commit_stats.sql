46f162497dc236fdbac3c88701d2d49ca9ed03dd
Richellere Yao <richellere.yao@dentsuaegis.com>
2019-11-20 15:29:29 +0100

    Code reformating...

--- src/AdminScripts/commit_stats.sql
+++ src/AdminScripts/commit_stats.sql
@@ -13,7 +13,11 @@ Notes:
 History:
 2015-02-09 ericfe created
 **********************************************************************************************/
-select startqueue,node, datediff(ms,startqueue,startwork) as queue_time, datediff(ms, startwork, endtime) as commit_time, queuelen 
-from stl_commit_stats 
-where startqueue >=  dateadd(day, -2, current_Date)
-order by queuelen desc , queue_time desc;
+select startqueue,
+       node,
+       datediff(ms, startqueue, startwork) as queue_time,
+       datediff(ms, startwork, endtime)    as commit_time,
+       queuelen
+from stl_commit_stats
+where startqueue >= dateadd(day, -2, current_Date)
+order by queuelen desc, queue_time desc;
